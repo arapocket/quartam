f6b294e97ee6757b8171179061a24506e8f7ba6a
arapocket <arakazaryan@me.com>
2019-02-12 19:56:45 -0800

    quicksave

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import * as actions from '../actions';
+
+const styles = {
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 15,
+  },
+};
 
 
 class ListItem extends Component {
@@ -8,14 +17,19 @@ class ListItem extends Component {
 
 
   render() {
+    const { titleStyle } = styles;
     const { library } = this.props;
+    console.log(this.props);
     return (
       <CardSection>
-        <Text>{library.item.title }</Text>
+        <Text style={titleStyle}>
+          {library.item.title }
+
+        </Text>
       </CardSection>
     );
   }
 }
 
 
-export default ListItem;
+export default connect(null, actions)(ListItem);
